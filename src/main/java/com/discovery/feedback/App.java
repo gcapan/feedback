c247b1baf8077d3d22f19247596f8760740b467f
gcapan <gkhn5489633>
2013-07-03 16:03:03 +0300

    initial maven-quickstart archetype

---
+++ src/main/java/com/discovery/feedback/App.java
@@ -0,0 +1,13 @@
+package com.discovery.feedback;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
