b74e683ef43945a8ea5ecf8abd6a489d925bbdd2
Ismail Keskin <iskeskin@gmail.com>
2013-07-04 17:13:45 +0300

    Embed Glassfish (which can be executed by 'mvn embedded-glassfish run')

---
+++ feedback-rest/src/main/java/com/discovery/feedback/rest/RestApplication.java
@@ -0,0 +1,8 @@
+package com.discovery.feedback.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestApplication extends Application {
+}
---
+++ feedback-rest/src/main/java/com/discovery/feedback/rest/Users.java
@@ -0,0 +1,16 @@
+package com.discovery.feedback.rest;
+
+import com.discovery.feedback.model.HBaseDataModel;
+import org.apache.mahout.cf.taste.common.TasteException;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("users")
+public class Users {
+
+  @GET
+  public String getNumItems() throws TasteException {
+    return String.valueOf(new HBaseDataModel().getNumItems());
+  }
+}
